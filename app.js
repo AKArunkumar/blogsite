60e4b9d325661e31b266dce48fabf0422cf62684
Arunkumar <AKArunkumar@users.noreply.github.com>
2018-03-14 22:23:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,148 +1,200 @@
-
-//require packages
 var express = require('express'),
-    expressSanitizer = require('express-sanitizer');
-    app = express(),
     bodyParser = require('body-parser'),
     path = require('path'),
-    request = require('request'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    localStrategy = require('passport-local'),
+    authusers = require('./models/authusers'),
+    user = require('./models/user'),
+    comments = require('./models/comments'),
+    seedDB = require('./seed');
 
-//static to public directory
+// seedDB();
+
+var app = express();
+
+//intilize pot
+app.set('port', 8082);
+
+// Serving static file
 app.use(express.static('public'));
-//set ejs as view engine
+
+
+// use viewengine to views
 app.set('view engine', 'ejs');
 
-//parsing request using bodyparser
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-app.use(bodyParser.json());
-//app use in sanitizer
-app.use(expressSanitizer());
 
+// parse application/json
+app.use(bodyParser.json())
 
-//Mongodb setup 
-//----------------------------------------------------
-mongoose.connect('mongodb://127.0.0.1/blogdb', {
+//passport configuration
+app.use(require('express-session')({
+    secret: "Don't tell anyone",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(authusers.authenticate()));
+passport.serializeUser(authusers.serializeUser());
+passport.deserializeUser(authusers.deserializeUser());
+
+// mongoose connection
+mongoose.connect('mongodb://127.0.0.1/campground', {
     useMongoClient: true
 })
 
+//settting loggedin  user 
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
 mongoose.Promise = global.Promise;
 
-var schema = mongoose.Schema({
-    title: String,
-    image: String,
-    body: String,
-    created: {
-        type: Date,
-        default: Date.now
+//login middleware function
+function isLoggedin(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
     }
-});
+    res.redirect('/login');
+}
 
-var blogdb = mongoose.model('blogdb', schema);
-//------------------------------------------------------
-//example
-// blogdb.create({
-//     title: "pet",
-//     image: "https://i.imgur.com/8pTwPlXb.jpg",
-//     body: "this dogs are so cute"
-//     created:
-//     oneh:{type:double, default:0}
-//     twoh:{tuype:double, default:0}
-// });
-
-//RESTfull Routes
-app.get('/',(req,res)=>{
-    res.redirect('/blogs')
-}) 
-
-app.get('/blogs', (req, res) => {
-    blogdb.find({})
-        .exec()
-        .then(function (blogs){
-            // res.send(blogs);
-            // console.log(blogs);
-            res.render("blog", {
-                blogs : blogs
-            })
-        }).catch(function(err){
-            console.error(err);
-            res.render("error");
-        });   
-});
-
-app.get('/blogs/add', function(req,res){
-    res.render('new');
-
+// Routes
+app.get('/', function (req, res) {
+    res.redirect('/campground');
 })
 
-app.post('/blogs/new', (req,res)=>{
-    req.body.blog.body = req.sanitize(req.body.blog.body);
-    blogdb.create(req.body.blog, function(err,result){
-        if(err){
-            console.error(err);
-            res.redirect('/blogs');
-        }
-        // console.log(result);
-        res.redirect('/blogs');
-    })
-});
-
-app.get('/blogs/:id', function(req,res){
-    let id = req.params.id;
-    blogdb.findById(id, function(err, result){
-        if(err){
-            console.error(err);
-            res.redirect("/blogs");
-        }
-        // console.log(result);
-        res.render("show", {
-            blog:result
+app.get('/campground', function (req, res) {
+    user.find({})
+        .then((users) => {
+            // console.log(users);
+            res.render('campground', {
+                users: users,
+                currentUser: req.user
+            });
         })
-    })
-});
-
-app.get('/blogs/:id/delete', function(req,res){
-    let id = req.params.id;
-    blogdb.findByIdAndRemove(id).then(function(rem){
-        console.log(rem);
-        console.log("then");
-        res.redirect('/blogs');
-    }).catch(function(err){
-        console.error(err);
-        console.log('catch');
-        res.redirect("/blogs");        
-    })
-})
-
-app.get('/blogs/:id/edit', function(req,res){
-    let id = req.params.id;
-    blogdb.findById(id, function(err, result){
-        if(err){
+        .catch(function (err) {
             console.error(err);
-        }
-        console.log(result);
-        res.render("edit", {
-            blog:result
+            res.send(err)
         })
-    })
-    
+
 })
 
+app.get('/campground/add', (req, res) => {
+    res.render('addcampground');
+})
 
-app.post('/blogs/:id/edit', (req,res)=>{
-    req.body.blog.body = req.sanitize(req.body.blog.body);        
+app.post('/campground/add', function (req, res) {
+
+    user.create(req.body.userobj)
+        .then((newuser) => {
+            console.log("new user is added");
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+
+    res.redirect('/campground');
+})
+
+app.get('/campground/show/:id', function (req, res) {
     let id = req.params.id;
-    blogdb.update({_id:id}, req.body.blog, function(err,suc){
-        if(err){
-            res.send(err);
-        }
-        res.redirect('/blogs');
-    })
+    user.findById(id)
+        .then((userinf) => {
+            console.log("user info is fetched");
+            let commentarr = [];
+            userinf.comments.forEach(function (comment_id) {
+                comments.findById(comment_id)
+                    .then(function (commenter) {
+                        console.log("commenter is fetched");
+                        commentarr.push(commenter);
+                        console.log(commentarr);
+                        res.render('showcampground', {
+                            userinf: userinf,
+                            comments: commentarr
+                        });
+                    })
+            });
+
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+
+})
+
+//user signup
+app.get('/register', function (req, res) {
+    res.render('register');
+})
+
+app.post('/register', function (req, res) {
+    var newuser = new authusers({
+        username: req.body.username
     });
+    authusers.register(newuser, req.body.password, function (err, use) {
+        console.log("above if");
+        if (err) {
+            console.log("In if");
+            console.log(err);
+            res.redirect('/register')
+        }
+        res.redirect('/campground');
+    })
+});
 
-//listen port assign
-app.listen('8080', () => {
-    console.log("Listening at port 8080");
+app.get('/login', function (req, res) {
+    res.render('login');
 })
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/campground',
+    failureRedirect: '/login'
+}), function (req, res) {
+
+})
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/campground');
+})
+
+app.get('/addcomment/:id', isLoggedin, function (req, res) {
+    let id = req.params.id;
+    res.render('addcomment', {
+        id: id
+    });
+})
+
+app.post('/addcomment/:id', function (req, res) {
+    let id = req.params.id;
+    console.log(id);
+    comments.create(req.body.comments)
+        .then(function (comment) {
+            console.log("comment is added");
+            user.findById(id)
+                .then(function (rtuser) {
+                    console.log(rtuser);
+                    rtuser.comments.push(comment._id);
+                    rtuser.save()
+                        .then(function (user) {
+                            console.log("user with comment is added");
+                            res.redirect('/campground/show/' + id);
+                        })
+                        .catch(function (err) {
+                            console.error(err);
+                        })
+                })
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+})
+
+app.listen(app.get('port'), () => {
+    console.log('server started........');
+});
